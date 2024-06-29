60c44a28d2b78b18af3fbff84c5a37688869b3b5
Sebastian Kolb <sebastiankolb@me.com>
2024-06-29 10:21:27 +0200

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Demo } from "./components/demo";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return <Demo />;
 }
 
