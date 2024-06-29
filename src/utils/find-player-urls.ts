f88d205af040ec72de62127935f3204a2f6d86cc
Sebastian Kolb <sebastiankolb@me.com>
2024-06-29 10:53:25 +0200

    Don't search for empty string

--- src/utils/find-player-urls.ts
+++ src/utils/find-player-urls.ts
@@ -1,7 +1,8 @@
 import urls from "../player-urls/player_urls.json";
 
 export const findPlayerUrls = (name: string) => {
-  console.log("name", name);
+  if (name === "") return [];
+
   const playerUrls = urls.filter((url) => url.includes(name));
 
   return playerUrls;
